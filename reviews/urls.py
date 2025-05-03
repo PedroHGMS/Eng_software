728d5860a384b4fb73a6b138f560fd2bbc0c2250
PedroHGMS <pedrom072001@gmail.com>
2025-05-03 15:52:22 -0300

    Revert "Merge branch 'homolog' into pedro"
    This reverts commit ebe56d1ef31aaf05a60dfc78eee0460472b61ecf, reversing
    changes made to 609285f23f1f44f2ff8f8b7ce61cdfedec1f69c1.

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,5 @@ from reviews import views
 app_name = "reviews"
 urlpatterns = [
     path("all_reviews/", views.all_reviews, name="all_reviews"),
-    path("professor_reviews/<int:professor_id>/", views.professor_reviews_view, name="professor_reviews"),
+    path("single_professor_reviews/<int:professor_id>/", views.single_professor_reviews, name="single_professor_reviews"),
 ]

6fa3aaf8ba9a876f7329729868eac518143d9691
Arthur <arthurfreisouza@gmail.com>
2025-05-02 22:48:13 -0300

    Saving my local changes.

--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import Review
 from universities.models import Professor
 from django.http import HttpResponse
+from django.db.models import Q
+
 
 # Loads the reviews from the database and renders them in the template
 def all_reviews(request):
@@ -21,3 +23,19 @@ def single_professor_reviews(request, professor_id):
 
     # Render the reviews in the template
     return render(request, "reviews/single_professor_reviews.html", {"reviews": reviews, "professor": professor})
+
+def search_reviews(request):
+    query = request.GET.get('q', '')
+    results = []
+
+    if query:
+        results = Review.objects.filter(
+            Q(professor__name__icontains=query) |  # professor name
+            Q(disciplina__nome__icontains=query) |  # discipline name
+            Q(descricao__icontains=query)           # review description
+        ).distinct()
+
+    return render(request, 'reviews/search_results.html', {
+        'query': query,
+        'results': results
+    })

b52b9335bff89c82bb913bdb17cf1c3756db1c74
guilhermepontelos <guilhermepontelos@gmail.com>
2025-05-04 21:30:31 -0300

    login logout

--- prof_review/urls.py
+++ prof_review/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for prof_review project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -22,8 +6,8 @@ from reviews import views as reviews_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews_views.all_reviews, name='home'),
     path('universities/', include("universities.urls", namespace="universities")),
     path('reviews/', include("reviews.urls", namespace="reviews")),
+    path('', include('users.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- universities/views.py
+++ universities/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .models import Professor
 
-
+@login_required
 def all_professors(request):
     """
     View to display all professors.
